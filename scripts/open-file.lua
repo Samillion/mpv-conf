bee591747c8be3464bd89ae2118993eb9670956b
Sam <17427046+Samillion@users.noreply.github.com>
2026-03-03 05:23:41 +0200

    scripts: add open-file.lua

---
+++ scripts/open-file.lua
@@ -0,0 +1,52 @@
+-- A fork of https://github.com/rossy/mpv-open-file-dialog
+-- Open file dialoge on Windows with a keybind
+-- Default is ctrl+o
+-- Can be assigned in menu.conf as well with:
+-- &File Explorer	script-binding open_file/open-file
+
+local utils = require 'mp.utils'
+
+function open_file()
+    local was_ontop = mp.get_property_native("ontop")
+    if was_ontop then
+        mp.set_property_native("ontop", false)
+    end
+
+    local res = utils.subprocess({
+        args = {
+            'powershell',
+            '-NoProfile',
+            '-STA',
+            '-Command',
+            [[
+            Add-Type -AssemblyName PresentationFramework
+            $ofd = New-Object Microsoft.Win32.OpenFileDialog
+            $ofd.Multiselect = $true
+            $ofd.Filter = "Media files|*.mkv;*.mp4;*.mp3;*.flac;*.avi|All files|*.*"
+
+            if ($ofd.ShowDialog() -eq $true) {
+                $ofd.FileNames -join "`n"
+            }
+            ]]
+        },
+        cancellable = false,
+        capture_stdout = true,
+        capture_stderr = true,
+    })
+
+    if was_ontop then
+        mp.set_property_native("ontop", true)
+    end
+
+    if not res or res.status ~= 0 or not res.stdout then
+        return
+    end
+
+    local first = true
+    for filename in string.gmatch(res.stdout, "[^\r\n]+") do
+        mp.commandv("loadfile", filename, first and "replace" or "append")
+        first = false
+    end
+end
+
+mp.add_key_binding('ctrl+o', 'open-file', open_file)
